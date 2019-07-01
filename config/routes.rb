94235846ca5dbb2cf70ceb497a270e7a48de68c5
Luis Enrique Silva Martínez <silmar.dll@gmail.com>
2019-07-01 14:43:08 -0500

    Refactoring routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   scope :api do
-    get '/books', to: "books#index"
-    get '/books/:id', to: "books#show", as: :book
-    post '/books', to: "books#create"
-    patch '/books/:id', to: "books#update"
-    delete '/books/:id', to: "books#destroy"
+    resources :books
   end
 end
